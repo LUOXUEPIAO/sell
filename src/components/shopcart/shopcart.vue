44ee90706cd0096adde88f5564205c267b3c0b9e
lxp <1715926634@qq.com>
2020-06-08 17:13:02 +0800

    update shopcarts

---
+++ src/components/shopcart/shopcart.vue
@@ -0,0 +1,149 @@
+<template>
+    <div class="shopcart">
+        <div class="content">
+            <div class="content-left">
+                <div class="logo-wrapper">
+                    <div class="logo" :class="{'highLight' : totalCount > 0}">
+                        <i class="icon-shopping_cart" :class="{'highLight' : totalCount > 0}"></i>
+                    </div>
+                    <div class="num" v-show="totalCount > 0">{{totalCount}}</div>
+                </div>
+                <div class="price" :class="{'highLight' : totalPrice > 0}">￥{{totalPrice}}</div>
+                <div class="desc">另需配送费￥{{deliveryPrice}}元</div>
+            </div>
+            <div class="content-right">
+                <div class="pay">
+                    ￥{{minPrice}}元起送
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+
+export default {
+props: {
+    selectFoods: {
+      type: Array,
+      default () {
+          return [
+             { price: 10,
+               count: 1
+             }
+          ]
+      }
+    },
+    deliveryPrice: {
+        type: Number,
+        default: 0},
+    minPrice: {
+        type: Number,
+        default: 0
+        }
+        },
+        computed: {
+            totalPrice () {
+                let total = 0
+                this.selectFoods.forEach((food) => {
+                    total += food.price * food.count
+                })
+                return total
+            },
+            totalCount () {
+               let count = 0
+               this.selectFoods.forEach((food) => {
+                   count += food.count
+               })
+               return count
+            }
+        }
+}
+</script>
+<style lang="stylus" scoped>
+.shopcart
+  position :fixed
+  left:0
+  bottom:0
+  z-index:50
+  width:100%
+  height:48px
+  background:#eee
+  .content
+      display:flex
+      background:#141d27
+      font-size:0
+      .content-left
+          flex:1
+          .logo-wrapper
+              display:inline-block
+              position:relative
+              margin:0 12px
+              padding:6px
+              top:-10px
+              width:56px
+              height:56px
+              box-sizing:border-box
+              vertical-align:top
+              border-radius:50%
+              background:#141d27
+              .num
+                  position :absolute
+                  top:0
+                  right:0
+                  width:16px
+                  height:16px
+                  line-height:16px
+                  font-size:9px
+                  font-weight:700
+                  color:#fff
+                  background:rgb(240,20,20)
+                  box-shadow:0 4px 8px 0 rgba(0,0,0,0.4)
+                  border-radius:16px
+                  text-align:center
+              .logo
+                  width:100%
+                  height:100%
+                  border-radius:50%
+                  background:#2b343c
+                  &.highLight
+                    background:rgb(0,160,220)
+                  text-align:center
+                  .icon-shopping_cart
+                      font-size:24px
+                      color:#80858a
+                      line-height:44px
+                      &.highLight
+                          color :#fff
+          .price
+              display:inline-block
+              vertical-align :top
+              line-height :24px
+              margin-top:12px
+              box-sizing:border-box
+              padding-right:12px
+              border-right :1px solid rgba(255,255,255,0.1)
+              font-size :16px
+              font-weight:700
+              color :rgba(255,255,255,0.4)
+              &.highLight
+                color :#fff
+          .desc
+              display:inline-block
+              vertical-align :top
+              line-height :24px
+              margin:12px 0 0 12px
+              color :rgba(255,255,255,0.4)
+              font-size :10px
+      .content-right
+          flex:0 0 105px
+          width:105px
+          .pay
+              line-height :48px
+              height :48px
+              text-align :center
+              font-size :12px
+              color :rgba(255,255,255,0.4)
+              font-weight:700
+              background :#2b333b
+
+</style>
--- src/views/goods.vue
+++ src/views/goods.vue
@@ -1,16 +1,45 @@
 <template>
     <div class="goods">
-      <div class="menu-wrapper">
-          <li v-for="(item, index) in goods" :key="index" class="menu-item">
+      <div class="menu-wrapper" ref="menuWrapper">
+          <ul>
+          <li v-for="(item, index) in goods" :key="index" class="menu-item" :class="{'current': currentIndex === index}" @click="selectMenu(index)">
               <span class="text border-1px" >
                   <span v-show="item.type > 0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
               </span>
           </li>
+          </ul>
       </div>
-      <div class="foods-wrapper"></div>
+      <div class="foods-wrapper" ref="foodsWrapper">
+          <ul>
+              <li v-for="(item, index) in goods" :key="index" class="food-list food-list-hook">
+                  <h1 class="title">{{item.name}}</h1>
+                  <ul>
+                      <li v-for="(food, food_index) in item.foods" :key="food_index" class="food-item border-1px">
+                          <div class="icon">
+                              <img :src="food.icon" alt="" width="57px" height="57px">
+                          </div>
+                          <div class="content">
+                              <h2 class="name">{{food.name}}</h2>
+                              <p class="desc">{{food.description}}</p>
+                              <div class="extra">
+                                  <span class="count">月售{{food.sellCount}}份</span>
+                                  <span>好评率{{food.rating}}%</span>
+                              </div>
+                              <div class="price">
+                                  <span class="now">￥{{food.price}}</span><span class="old" v-show='food.oldPrice'>￥{{food.oldPrice}}</span>
+                              </div>
+                          </div>
+                      </li>
+                  </ul>
+              </li>
+          </ul>
+      </div>
+      <shopcart :deliveryPrice="seller.deliveryPrice" :min-price="seller.minPrice"></shopcart>
     </div>
 </template>
 <script>
+import BScroll from 'better-scroll'
+import shopcart from '../components/shopcart/shopcart'
 const ERR_OK = 0
 export default {
     props: {
@@ -18,7 +47,9 @@ export default {
     },
     data () {
         return {
-          goods: []
+          goods: [],
+          listHeight: [],
+          scrollY: 0
         }
     },
     created () {
@@ -27,8 +58,60 @@ export default {
             res = res.body
             if (res.errno === ERR_OK) {
                   this.goods = res.data
+                  this.$nextTick(() => {
+                     this._initScroll()
+                     this._calculateHight()
+                  })
             }
         })
+    },
+    computed: {
+        currentIndex () {
+          for (let i = 0; i < this.listHeight.length; i++) {
+              let height1 = this.listHeight[i]
+              let height2 = this.listHeight[i + 1]
+              if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+                  return i
+              }
+          }
+          return 0
+        }
+    },
+    methods: {
+       _initScroll () {
+         this.menuScroll = new BScroll(this.$refs.menuWrapper, {
+             click: true
+         })
+         this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {
+             probeType: 3
+         })
+         // 注册事件，当右侧列表滚动的时候，实时的拿到scrolly值
+         this.foodsScroll.on('scroll', (pos) => {
+             this.scrollY = Math.abs(Math.round(pos.y))
+         })
+       },
+       _calculateHight () {
+           let foodList = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook')
+        //    console.log(foodList)
+        //    console.log(this.$refs.foodsWrapper)
+        let height = 0
+        this.listHeight.push(height)
+        for (let i = 0; i < foodList.length; i++) {
+          let item = foodList[i]
+          height += item.clientHeight
+          this.listHeight.push(height)
+        }
+      //  console.log(this.listHeight) 拿到每个li的高度
+       },
+       selectMenu (index) {
+           let foodList = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook')
+           let el = foodList[index]
+           this.foodsScroll.scrollToElement(el, 300)
+           // console.log(index)
+       }
+    },
+    components: {
+        shopcart
     }
 }
 </script>
@@ -51,6 +134,14 @@ export default {
             width:56px
             line-height :14px
             padding :0 12px
+            &.current
+                font-weight:700
+                position :relative
+                z-index :10
+                margin-top :-1px
+                background :#fff
+                .text
+                    border-none()
             .icon
                 vertical-align :top
                 display :inline-block
@@ -77,4 +168,52 @@ export default {
                 border-1px(rgba(7,17,27,0.1))
     .foods-wrapper
         flex :1
+        .title
+            padding-left:14px
+            height :26px
+            line-height :26px
+            border-left:2px solid #d9dde1
+            font-size :12px
+            color :rgb(147,153,159)
+            background :#f3f5f7
+        .food-item
+            display :flex
+            margin :18px
+            padding-bottom :18px
+            border-1px(rgba(7,17,27,0.1))
+            &:last-child
+                border-none()
+                margin-bottom :0
+            .icon
+                flex :0 0 57px
+                margin-right :10px
+            .content
+                flex:1
+                .name
+                    margin :2px 0 8px 0
+                    height :14px
+                    line-height :14px
+                    font-size :14px
+                    color :rgb(7,17,27)
+                .desc, .extra
+                    line-height :10px
+                    font-size:10px
+                    color:rgb(147,153,159)
+                .desc
+                    line-height :12px
+                    margin-bottom :8px
+                .extra
+                    .count
+                        margin-right:12px
+                .price
+                    font-weight:700
+                    line-height :24px
+                    .now
+                        margin-right:8px
+                        font-size :14px
+                        color :rgb(240,20,20)
+                    .old
+                        text-decoration :line-through
+                        font-size :10px
+                        color :rgb(147,153,159)
 </style>
